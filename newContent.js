edf37f3efe6993371266f993278a762bb041d062
Jake Labate <jake@labatemarketing.com>
2022-12-08 20:36:15 -0500

    see diff

--- newContent.js
+++ newContent.js
@@ -1,24 +1,6 @@
 console.log("newContent.js loaded");
 
-// Output Content Tabs
-function openCity(evt, outputTabName) {
-	let i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-	tabcontent[i].style.display = "none";
-}
-	tablinks = document.getElementsByClassName("socialTab");
-	for (i = 0; i < tablinks.length; i++) {
-	tablinks[i].className = tablinks[i].className.replace(" active", "");
-}
-	document.getElementById(outputTabName).style.display = "block";
-	evt.currentTarget.className += " active";
-
-}
-
 // Screenshot
-// Input: <div id="photo"><button onclick="screenshot();">Take Screenshot</button></div>
-// Output: <div id="output"></div>
 function downloadFinalTextBlob() {
 
 	// Get the content
